8db875a787896a1719206f16e41388c0a1c91368
Alfred Luo <237853913@qq.com>
2020-03-21 16:03:33 +0800

    202003211603

--- guawaDemo/src/main/java/org/xiaofengcanyue/collections/CollectionUtilities.java
+++ guawaDemo/src/main/java/org/xiaofengcanyue/collections/CollectionUtilities.java
@@ -1,13 +1,11 @@
 package org.xiaofengcanyue.collections;
 
 import com.google.common.base.Function;
+import com.google.common.base.Supplier;
 import com.google.common.collect.*;
 import com.google.common.primitives.Ints;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Interface  JDK or Guava?	  Corresponding Guava utility class
@@ -93,6 +91,116 @@ public class CollectionUtilities {
                 return string.length();
             }
         });
+
+        Map<String, Integer> left = ImmutableMap.of("a", 1, "b", 2, "c", 3);
+        Map<String, Integer> right = ImmutableMap.of("b", 2, "c", 4, "d", 5);
+        MapDifference<String, Integer> diff = Maps.difference(left, right);
+
+        diff.entriesInCommon(); // {"b" => 2}
+        diff.entriesDiffering(); // {"c" => (3, 4)}
+        diff.entriesOnlyOnLeft(); // {"a" => 1}
+        diff.entriesOnlyOnRight(); // {"d" => 5}
+    }
+
+    /**
+     * The Guava utilities on BiMap live in the Maps class, since a BiMap is also a Map.
+     */
+    public static void AboutBiMap(){
+        Maps.synchronizedBiMap(HashBiMap.create());
+        Maps.unmodifiableBiMap(HashBiMap.create());
+    }
+
+    public static void AboutMultiSets(){
+        Multiset<String> multiset1 = HashMultiset.create();
+        multiset1.add("a", 2);
+
+        Multiset<String> multiset2 = HashMultiset.create();
+        multiset2.add("a", 5);
+
+        multiset1.containsAll(multiset2); // returns true: all unique elements are contained,
+        // even though multiset1.count("a") == 2 < multiset2.count("a") == 5
+        Multisets.containsOccurrences(multiset1, multiset2); // returns false
+
+        Multisets.removeOccurrences(multiset1,multiset2); // multiset2 now contains 3 occurrences of "a"
+
+        multiset2.removeAll(multiset1); // removes all occurrences of "a" from multiset2, even though multiset1.count("a") == 2
+        multiset2.isEmpty(); // returns true
+
+        Multiset<String> multiset = HashMultiset.create();
+        multiset.add("a", 3);
+        multiset.add("b", 5);
+        multiset.add("c", 1);
+
+        ImmutableMultiset<String> highestCountFirst = Multisets.copyHighestCountFirst(multiset);
+
+        Multisets.unmodifiableMultiset(multiset);
+        Multisets.unmodifiableSortedMultiset(TreeMultiset.create());
+    }
+
+    public static void AboutMultimaps(){
+        ImmutableSet<String> digits = ImmutableSet.of(
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine");
+        Function<String, Integer> lengthFunction = new Function<String, Integer>() {
+            public Integer apply(String string) {
+                return string.length();
+            }
+        };
+        ImmutableListMultimap<Integer, String> digitsByLength = Multimaps.index(digits, lengthFunction);
+        /*
+         * digitsByLength maps:
+         *  3 => {"one", "two", "six"}
+         *  4 => {"zero", "four", "five", "nine"}
+         *  5 => {"three", "seven", "eight"}
+         */
+
+
+        ArrayListMultimap<String, Integer> multimap = ArrayListMultimap.create();
+        multimap.putAll("b", Ints.asList(2, 4, 6));
+        multimap.putAll("a", Ints.asList(4, 2, 1));
+        multimap.putAll("c", Ints.asList(2, 5, 3));
+
+        TreeMultimap<Integer, String> inverse = Multimaps.invertFrom(multimap, TreeMultimap.<Integer,String>create());
+        // note that we choose the implementation, so if we use a TreeMultimap, we get results in order
+        /*
+         * inverse maps:
+         *  1 => {"a"}
+         *  2 => {"a", "b", "c"}
+         *  3 => {"c"}
+         *  4 => {"a", "b"}
+         *  5 => {"c"}
+         *  6 => {"b"}
+         */
+
+        Map<String, Integer> map = ImmutableMap.of("a", 1, "b", 1, "c", 2);
+        SetMultimap<String, Integer> multimap1 = Multimaps.forMap(map);
+        // multimap maps ["a" => {1}, "b" => {1}, "c" => {2}]
+        Multimap<Integer, String> inverse1 = Multimaps.invertFrom(multimap1, HashMultimap.<Integer, String> create());
+        // inverse maps [1 => {"a", "b"}, 2 => {"c"}]
+
+
+        ListMultimap<String, Integer> myMultimap = Multimaps.newListMultimap(
+                Maps.<String, Collection<Integer>>newTreeMap(),
+                new Supplier<LinkedList<Integer>>() {
+                    public LinkedList<Integer> get() {
+                        return Lists.newLinkedList();
+                    }
+                });
+    }
+
+    public static void AboutTables(){
+        // use LinkedHashMaps instead of HashMaps
+        Table<String, Character, Integer> table = Tables.newCustomTable(
+                Maps.<String, Map<Character, Integer>>newLinkedHashMap(),
+                new Supplier<Map<Character, Integer>> () {
+                    public Map<Character, Integer> get() {
+                        return Maps.newLinkedHashMap();
+                    }
+                });
+
+        Tables.transpose(table);
+
+        Tables.unmodifiableTable(table);
     }
 
 }
