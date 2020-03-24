33f80c71f8ac343eab83b5f7abea433989ae4e16
Alfred Luo <237853913@qq.com>
2020-03-24 16:15:30 +0800

    202003241614

---
+++ guawaDemo/src/main/java/org/xiaofengcanyue/functionalutilities/AbountFunctionalUtilities.java
@@ -0,0 +1,16 @@
+package org.xiaofengcanyue.functionalutilities;
+
+/**
+ * Guava提供两个基本functional接口：
+ *   1、Function<A,B>只有一个方法 B apply(A input)
+ *   2、Predicate<T>只有一个方法 boolean apply(T input)。
+ */
+public class AbountFunctionalUtilities {
+
+    /**
+     * Characters有一个特别的Predicate——CharMatcher。
+     *
+     * 此外，对于comparable types和comparison-based predicates，多数需求Range类型都可以满足。
+     *
+     */
+}
